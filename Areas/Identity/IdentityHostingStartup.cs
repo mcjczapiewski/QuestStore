7e277239b5976af4b9aae0ffa8b72cbb0c0e360a
budi8 <budi8@STRIX>
2020-10-07 18:44:18 +0200

    Added ASP.net identity

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using QuestStore.Data;
+
+[assembly: HostingStartup(typeof(QuestStore.Areas.Identity.IdentityHostingStartup))]
+namespace QuestStore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
