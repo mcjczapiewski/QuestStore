0662e8daa4d29ed3ea38eb8caec636c489384daf
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-10-07 21:31:18 +0200

    Add connection to MySQL DB

---
+++ Models/QuestDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace QuestStore.Models
+{
+    public class QuestDBContext : DbContext
+    {
+        public DbSet<user> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseMySql(@"server=91.192.164.15;uid=horizonp_cc-queststore;pwd=queststoreFORlajf;Database=horizonp_cc-queststore");
+        }
+    }
+}
