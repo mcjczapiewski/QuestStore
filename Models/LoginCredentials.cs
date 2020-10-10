2b2d99811d6bd6e74639cac76af38a73e5751957
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-10-10 15:54:20 +0200

    Add connection to credentials database

---
+++ Models/LoginCredentials.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestStore.Models
+{
+    public class LoginCredentials
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
