7cebb2b1b9d0f263d200c914413c890bdc85ebae
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-10-24 19:46:06 +0200

    Change user login method from email to name login, split email during registration

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -77,7 +77,7 @@ namespace QuestStore.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new IdentityUser { UserName = Input.Email.Split("@")[0], Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
