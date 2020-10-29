13d09473c3d1ea259e5d4325557612c963cc8784
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-10-29 19:02:03 +0100

    Add MyQuests View, move quests table to partial

--- Controllers/QuestsController.cs
+++ Controllers/QuestsController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using QuestStore.Models;
@@ -23,6 +26,18 @@ namespace QuestStore.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.Quests.ToListAsync());
+        }
+
+        // GET: Quests/MyQuests
+        public async Task<IActionResult> MyQuests()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var quests = await _context.Users
+                .Where(i => i.CredentialsId == userId)
+                .SelectMany(q => q.UsersQuests)
+                .Select(qs => qs.Quest)
+                .ToListAsync();
+            return View(quests);
         }
 
         // GET: Quests/Details/5
